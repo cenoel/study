036fc42e972943c171b7206fe708cfdb6d4bfb22
Razafimiandrisoa Noarison Léonce <trazafimiandrisoa@gmail.com>
2018-08-17 15:03:52 +0300

    init start

--- start_product/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import start_product_models
--- start_product/models/start_product_models.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class start_product(models.Model):
-    _inherit = 'product.template'
